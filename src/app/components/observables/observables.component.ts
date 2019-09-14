cf94771eb14ead2c7cb65167fa50350f7251c4ec
arnabbhat82 <arnabbhat@gmail.com>
2019-09-14 19:04:50 +0530

    fix(rxjs): minimised code

--- src/app/components/observables/observables.component.ts
+++ src/app/components/observables/observables.component.ts
@@ -1,46 +1,30 @@
 import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
-import { of, Subject, BehaviorSubject, ReplaySubject } from 'rxjs';
+import { of, Subject } from 'rxjs';
 
 @Component({
-	template: '',
-	changeDetection: ChangeDetectionStrategy.OnPush,
+  template: '',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ObservablesComponent implements OnInit {
-	// Observable
-	readonly observable$ = of([1, 2, 3, 4, 5]);
-	// Subject
-	private readonly subject = new Subject<number>();
-	readonly subject$ = this.subject.asObservable();
-	// BehaviorSubject
-	private readonly behaviorSubject = new BehaviorSubject<number>(0);
-	readonly behaviorSubject$ = this.behaviorSubject.asObservable();
-	// ReplaySubject
-	private readonly replaySubject = new ReplaySubject<number>();
-	readonly replaySubject$ = this.replaySubject.asObservable();
+  // Observable
+  observable$ = of();
+  // Subject
+  subject = new Subject<number>();
 
-	ngOnInit() {
-		this.observableSubscribe();
-		// this.subjectSubscribe();
-		// this.vehaviorSubjectSubscribe();
-		// this.replaySubjectSubscribe();
-	}
+  ngOnInit() {
+    this.observableSubscribe();
+    this.subjectSubscribe();
+  }
 
-	observableSubscribe() {
-		this.observable$.subscribe(num => console.log('Observable obsA', num));
-		this.observable$.subscribe(num => console.log('Observable obsB', num));
-		this.observable$.subscribe(num => console.log('Observable obsC', num));
-	}
+  observableSubscribe() {
+    this.observable$ = of([1, 2, 3, 4, 5]);
+    this.observable$.subscribe(num => console.log('Observable obsA', num));
+    this.observable$.subscribe(num => console.log('Observable obsB', num));
+    this.observable$.subscribe(num => console.log('Observable obsC', num));
+  }
 
-	subjectSubscribe() {
-		this.subject$.subscribe(num => console.log('Subject subA', num));
-		this.subject.next(1);
-		this.subject$.subscribe(num => console.log('Subject subB', num));
-		this.subject.next(2);
-		this.subject$.subscribe(num => console.log('Subject subC', num));
-		this.subject.next(3);
-	}
-
-	vehaviorSubjectSubscribe() {}
-
-	replaySubjectSubscribe() {}
+  subjectSubscribe() {
+    this.subject.subscribe(num => console.log('Subject subA', num));
+    this.subject.next(1);
+  }
 }
